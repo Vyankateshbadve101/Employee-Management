c1aa9ff8125ce5e7fd7001912e3c11fd6ffd16cc
vyankatesh badve <73172151+Vyankateshbadve101@users.noreply.github.com>
2024-06-19 18:32:23 +0530

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Ajinkya',
+    lastName: 'Dude',
+    email: 'Ajinkyadude@gmail.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Arbaz',
+    lastName: 'Shaik',
+    email: 'ArbazShaik@gmail.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Prathmesh',
+    lastName: 'Sathe',
+    email: 'Prathameshsathe@gmail.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Rohit',
+    lastName: 'Namde',
+    email: 'Rohitnamde@gmail.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Vishnu',
+    lastName: 'Mugle',
+    email: 'Vishnumugle@gmail.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Amol',
+    lastName: 'Jagtap',
+    email: 'Amoljagtap@gmail.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'Gajanan',
+    lastName: 'Jagtap',
+    email: 'Gajananjagtap@gmail.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'Vaibhav',
+    lastName: 'Badve',
+    email: 'Vaibhavbadve99@gmail.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'Rushikesh',
+    lastName: 'Lagad',
+    email: 'Rushilagad@gmail.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'Shrirang',
+    lastName: 'Kendre',
+    email: 'Shreekendre@gmail.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
